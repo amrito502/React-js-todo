bede89cebb17d8d5d0707f4e83a2b27d0517da97
amrito502 <amritobosu7@gmail.com>
2024-05-13 16:58:58 +0600

    edit update and delete

--- src/App.js
+++ src/App.js
@@ -4,23 +4,58 @@ import "./App.css";
 function App() {
   const [todoTitle, setTodoTitle] = useState("");
   const [todoList, setTodoList] = useState([]);
+const [isEditable, setIsEditable] = useState(false);
+const [editableTodo, setEditableTodo] = useState(null);
 
   const createEventHandeler = (e) => {
     e.preventDefault();
-    setTodoList([todoTitle, ...todoList]);
+
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle
+    }
+
+
+    setTodoList([newTodo, ...todoList]);
     setTodoTitle("");
+  }
+
+  const editHandler = (id) => {
+    const tobeEditedTodo = todoList.find(todo => todo.id === id);
+    setIsEditable(true);
+    setEditableTodo(tobeEditedTodo);
+    setTodoTitle(tobeEditedTodo.title);
+  }
+
+  const updateHandler = (e) => {
+    e.preventDefault();
+    editableTodo.title = todoTitle;
+    setTodoTitle("");
+    setIsEditable(false);
+    setEditableTodo(null);
+  }
+
+  const deleteHandler = (id) => {
+    // const tobeDeletedTodo = todoList.find(todo => todo.id === id);
+    const newTodoList = todoList.filter(todo => todo.id !== id);
+    setTodoList(newTodoList);
   }
 
   return (
     <div className="App">
       <form className="form">
         <input required value={todoTitle} type="text" onChange={ (e) => setTodoTitle(e.target.value)} />
-        <button onClick={(e) => createEventHandeler(e)}>Create Data</button>
+        <button onClick={(e) => isEditable === true ? updateHandler(e) : createEventHandeler(e)}>
+          {isEditable === true ? "Update Todo" : "Create Todo"}
+        </button>
       </form>
       <div className="showData">
         <ul>
-          {todoList.map(item => (
-            <li>{item} <span className="edit">Edit</span><span className="delete">Delete</span></li>
+          {todoList.map(todo => (
+            <li>{todo.title} 
+            <span className="edits" onClick={() => editHandler(todo.id)}><button>Edit</button></span>
+            <span className="deletes" onClick={() => deleteHandler(todo.id)}><button>Delete</button></span>
+            </li>
           ))}
         </ul>
       </div>
